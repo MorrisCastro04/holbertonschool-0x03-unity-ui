9eec601eea3c210ae7093ae790447ba037f8260b
Morris Jomar <jomarcastro2004@gmail.com>
2024-09-05 16:28:45 -0400

    add two teleporters

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private bool isTeleport = false;
+    private string TeleporterName;
     public float speed;
     public int health = 5;
     private float MovementX, MovementY;
     private int score = 0;
+    private Vector3 otherTeleporter;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +22,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene("maze");
+        }
     }
 
     void FixedUpdate()
@@ -50,5 +58,26 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("You win!");
         }
+
+        if (other.CompareTag("Teleporter") && isTeleport == false)
+        {
+            if (other.name == "Teleporter")
+            {
+                transform.position = GameObject.Find("Teleporter (1)").transform.position;
+                TeleporterName = "Teleporter (1)";
+            }
+            else
+            {
+                transform.position = GameObject.Find("Teleporter").transform.position;
+                TeleporterName = "Teleporter";
+            }
+            isTeleport = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (TeleporterName == other.name)
+            isTeleport = false;
     }
 }
